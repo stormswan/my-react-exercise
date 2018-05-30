de9bad89668d986827e6a2874f71a3d70e2b16ff
Bob <bobhuipeng@gmail.com>
2018-05-31 00:12:35 +1000

    add redux devtools extension, solve the redux plugin did not show store infor in Chrome

--- src/reduxjs/index.js
+++ src/reduxjs/index.js
@@ -1,10 +1,11 @@
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import appReducer from './reducers'
 
 export default (initialState={}) => {
 	console.log(initialState);
-	var store = createStore(appReducer,initialState,applyMiddleware(thunk))
+	var store = createStore(appReducer,initialState,composeWithDevTools(applyMiddleware(thunk)))
 	console.log('store.getState()');
 	console.log(store.getState());
 	
