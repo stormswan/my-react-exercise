9647f0be9a8041fabf6ff0ec888856c6597e16f3
Bob <bobhuipeng@gmail.com>
2018-05-31 00:06:47 +1000

    Miss import

--- src/pages/reduxjs/index.js
+++ src/pages/reduxjs/index.js
@@ -1,5 +1,6 @@
 import {thunMiddleware} from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import appReducer from './reducers'
 
 export default (initialState={}) => {
